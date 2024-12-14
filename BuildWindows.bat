24a093b04fd267d2610c675d0e36914504b26731
TheLuke013 <araujochagaslucas2@gmail.com>
2024-12-14 18:26:02 -0300

    Changed to Debug build

--- BuildWindows.bat
+++ BuildWindows.bat
@@ -8,7 +8,7 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDe
 
 set SOLUTION_PATH=KingdomEngine.sln
 
-set CONFIGURATION=Release
+set CONFIGURATION=Debug
 set PLATFORM=x64
 
 msbuild %SOLUTION_PATH% /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM%
